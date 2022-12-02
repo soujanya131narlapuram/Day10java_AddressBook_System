ca563c13e303b01b9018ba1029c024b9c619d450
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-03 00:44:39 +0530

    'uc2 commit'

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
     public String firstName;
     public String lastName;
@@ -23,11 +25,27 @@ public class AddressBook {
     }
 
     public static void main(String[] args){
-        AddressBook obj1=new  AddressBook("Soujanya","Narlapuram",
-                "hn-034","hyd",500088,738257334,"123@gmail.com");
-        ArrayList<AddressBook> ads_book=new ArrayList<AddressBook>();
+        System.out.println("enter the details of new contact:");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("FirstName");
+        String ip_firstName=sc.nextLine();
+        System.out.println("lastName");
+        String ip_lastName=sc.nextLine();
+        System.out.println("address");
+        String ip_address=sc.nextLine();
+        System.out.println("City");
+        String ip_city=sc.nextLine();
+        System.out.println("gmail");
+        String ip_gmail=sc.nextLine();
+        System.out.println("ZipCode");
+        int ip_zipCode=sc.nextInt();
+        System.out.println("PhoneNumber");
+        long ip_phoneNumber=sc.nextLong();
+
+        AddressBook obj1=new  AddressBook(ip_firstName,ip_lastName,
+                ip_address,ip_city,ip_zipCode,ip_phoneNumber,ip_gmail);
+        ArrayList<AddressBook> ads_book=new ArrayList<>();
         ads_book.add(obj1);
-        System.out.println("Information stored into AddressBook through constructor parameters");
         System.out.println("Showing the information stored on the AddressBook");
         ads_book.get(0).display();
     }
